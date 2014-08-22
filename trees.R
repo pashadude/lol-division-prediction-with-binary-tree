639df1565c7f5677274208c2435d45568ec6a602
pashadude <pavel.dudko@gmail.com>
2014-08-22 05:14:45 +0400

    binary tree prediction added

--- trees.R
+++ trees.R
@@ -1,14 +1,17 @@
+install.packages("rattle")
+install.packages("rpart.plot")
+
 library(caret)
 library (kernlab)
 library(ggplot2)
-dataset <- read.csv("/home/proveyourskillz/Desktop/trees/userdata.csv", header=T, row.names=1)
+library(rattle)
+
+
+dataset <- read.csv("/home/proveyourskillz/Desktop/trees/userdatatree.csv", header=T, row.names=1)
 inTrain<-createDataPartition(dataset$League,p=3/4)[[1]]
 training<-dataset[inTrain,]
 testing<-dataset[-inTrain,]
 modFit<-train(League~.,method="rpart",data=training)
 print(modFit$finalModel)
-install.packages("rattle")
-install.packages("rpart.plot")
-library(rattle)
 fancyRpartPlot(modFit$finalModel)
-
+predict(modFit,newdata=testing)
